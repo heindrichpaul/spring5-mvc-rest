4440de49f673d114111a57300f47002c162f8b68
Heindrich Paul <heindrich.paul@gmail.com>
2020-09-18 11:26:28 +0200

    Fixed the lombok annotation that was missing

--- src/main/java/guru/springfamework/api/v1/model/CategoryDTO.java
+++ src/main/java/guru/springfamework/api/v1/model/CategoryDTO.java
@@ -1,8 +1,11 @@
 package guru.springfamework.api.v1.model;
 
+import lombok.Data;
+
 /**
  * Created by heindrichpaul on 18/09/2020
  */
+@Data
 public class CategoryDTO {
     private Long id;
     private String name;
